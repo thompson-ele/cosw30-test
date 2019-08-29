2846d6f036f60be7209756348a9181fd564d47f1
EC2 Default User <ec2-user@ip-172-31-81-40.ec2.internal>
2019-08-29 02:58:25 +0000

    Created lab 1 file and added bacon ipsum

---
+++ labwork/lab1.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <title>My First PHP Page</title>
+    </head>
+    <body>
+        <h1>Ele Thompson</h1>
+        <p>Bacon ipsum dolor amet biltong porchetta tail rump, 
+        landjaeger ball tip burgdoggen picanha. Pork chop chuck 
+        pancetta porchetta corned beef biltong hamburger. Beef 
+        flank short ribs, pork pig burgdoggen biltong cupim pork 
+        loin shank bresaola. Tenderloin venison doner sirloin 
+        pastrami pancetta tri-tip drumstick spare ribs bacon.</p>
+        
+        <?php 
+            echo '<a href="">This is my PHP beautiful paragraph</a>';
+        ?>
+    </body>
+</html>
