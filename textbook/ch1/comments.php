afd59e56e35aa88941457cff58281428a730e67b
EC2 Default User <ec2-user@ip-172-31-81-40.ec2.internal>
2019-09-04 04:51:25 +0000

    Worked on chapter 1 exercises

---
+++ textbook/ch1/comments.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Comments</title>
+    </head>
+    <body>
+        <?php
+        # Script 1.4 - comments.php
+        # Created September 3, 2019
+        # Created by Ele Thompson
+        # This script does nothing much.
+        
+        echo '<p>This is a line of text.
+              <br>This is another line of text.</p>';
+              
+        /*
+        echo 'This line will not be executed';
+        */
+        
+        echo "<p>Now I'm done.</p>";
+        // End of PHP code.
+        ?>
+    </body>
+</html>
---
+++ textbook/ch1/first.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Using Echo</title>
+    </head>
+    <body>
+        <!-- Script 1.2 - first.php -->
+        <p>This is standard HTML.</p>
+        <?php
+        echo 'This was generated using PHP!';
+        ?>
+    </body>
+</html>
